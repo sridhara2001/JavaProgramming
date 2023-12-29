dfbc37ca8bbd9c44f700f527e69b81fa65aa6714
sridhara2001 <srisridhara2001@gmail.com>
2023-12-29 14:18:04 +0530

    code

---
+++ Arrays/src/com/arrays/Problem4.java
@@ -0,0 +1,19 @@
+//package com.arrays;
+//
+//// Largest number in an array
+//public class Problem4 {
+//	public static int largestNum(int[] arr) {
+//		int max = arr[0];
+//		for(int i=1; i<arr.length; i++) {
+//			if(arr[i]>max) {
+//				return max;
+//			}
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
