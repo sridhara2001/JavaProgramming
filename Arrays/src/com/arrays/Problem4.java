ded6d78e44896cd18385e814b37ddf3000fcbfd1
sridhara2001 <srisridhara2001@gmail.com>
2023-12-29 19:52:26 +0530

    Code 29/12/2023

--- Arrays/src/com/arrays/Problem4.java
+++ Arrays/src/com/arrays/Problem4.java
@@ -1,19 +1,32 @@
-//package com.arrays;
-//
-//// Largest number in an array
-//public class Problem4 {
-//	public static int largestNum(int[] arr) {
-//		int max = arr[0];
-//		for(int i=1; i<arr.length; i++) {
-//			if(arr[i]>max) {
-//				return max;
-//			}
-//		}
-//	}
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
+package com.arrays;
+
+// Largest number in an array
+public class Problem4 {
+	public static int largestNum(int[] arr) {
+		int n1;
+		int n2;
+		int large=0;
+		for(int i=0; i<arr.length; i++) {
+			n1=arr[i];
+			
+			for(int j=0; j<arr.length; j++) {
+				n2=arr[j];
+				if(n1>n2) {
+					large=n1;
+				}else {
+					large=n2;
+					break;
+				}
+			}
+		}
+		return large;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,7,6,4,9,5,8};
+		int result = Problem4.largestNum(arr);
+		System.out.println("The Largest Number is "+result);
+
+	}
+
+}
