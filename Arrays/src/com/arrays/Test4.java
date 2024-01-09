09fa81b8a570e1b9a7b83cc58dbeb1bf5c85e4a6
sridhara2001 <srisridhara2001@gmail.com>
2024-01-09 14:07:16 +0530

    Code 09-01-2024

--- Arrays/src/com/arrays/Test4.java
+++ Arrays/src/com/arrays/Test4.java
@@ -3,14 +3,19 @@ package com.arrays;
 public class Test4 {
 
 	public static void main(String[] args) {
-		int[] arr = {1,5,10,25,15,20};
-		int bigNum=0;
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i]>bigNum) {
-				bigNum=arr[i];
+		int[] arr = {1,5,20,15,10};
+		int n = arr.length;
+		if(arr[0]>arr[1]) {
+			System.out.println("The index number is "+0+" and number is "+arr[0]);
+		}
+		if(arr[n-1]>arr[n-2]) {
+			System.out.println("The index number is "+(n-1)+" and number is "+arr[n-1]);
+		}
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i-1]<=arr[i] && arr[i]>=arr[i+1]) {
+				System.out.println("The index number is "+i+" and number is "+arr[i]);
 			}
 		}
-		System.out.println(bigNum);
 	}
 
 }
